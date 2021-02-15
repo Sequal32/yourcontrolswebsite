f98b706cb589fcd781b5d762f16eeb7188912d5c
SafeShows <nils3nils@gmail.com>
2021-02-15 13:27:31 +0200

    added vue-meta

--- yourcontrols.xyz/resources/js/app.js
+++ yourcontrols.xyz/resources/js/app.js
@@ -1,7 +1,10 @@
 import { App, plugin } from '@inertiajs/inertia-vue'
 import Vue from 'vue'
 import { InertiaProgress } from '@inertiajs/progress'
+import VueMeta from 'vue-meta'
 import './bootstrap'
+
+Vue.use(VueMeta)
 
 InertiaProgress.init()
 Vue.use(plugin)
@@ -9,6 +12,9 @@ Vue.use(plugin)
 const el = document.getElementById('app')
 
 new Vue({
+  metaInfo:{
+    titleTemplate: '%s | YourControls'
+  },
   render: h => h(App, {
     props: {
       initialPage: JSON.parse(el.dataset.page),
