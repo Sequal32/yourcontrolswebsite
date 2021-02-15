4eb1ee58a702663706f36bc4c4c484456e547d4e
SafeShows <nils3nils@gmail.com>
2021-02-15 13:22:52 +0200

    added bootstrap

--- yourcontrols.xyz/resources/js/app.js
+++ yourcontrols.xyz/resources/js/app.js
@@ -1,6 +1,7 @@
 import { App, plugin } from '@inertiajs/inertia-vue'
 import Vue from 'vue'
 import { InertiaProgress } from '@inertiajs/progress'
+import './bootstrap'
 
 InertiaProgress.init()
 Vue.use(plugin)
