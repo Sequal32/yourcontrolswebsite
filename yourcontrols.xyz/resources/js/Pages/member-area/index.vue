4ab98bcd1a8a43d505ab0054d7902fd533761183
SafeShows <nils3nils@gmail.com>
2021-03-10 15:46:47 +0200

    💄 added disclaimer on home on member-area

---
+++ yourcontrols.xyz/resources/js/Pages/member-area/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <member-layout :user="user">
+        This is under heavy development. Any suggestions would be nice. You can send them in #suggestions and tagging @WebDev or @SafeShows#2002
+    </member-layout>
+</template>
+
+<script>
+import memberLayout from "../../Layouts/memberLayout.vue";
+import axios from "axios";
+
+export default {
+    props: ["user"],
+    components: {
+        memberLayout
+    },
+    metaInfo: {
+        title: "Home"
+    },
+    data: () => ({}),
+
+    methods: {}
+};
+</script>
+
+<style></style>
