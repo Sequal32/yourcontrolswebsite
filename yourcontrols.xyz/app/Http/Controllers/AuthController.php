f2aa6de64a314a23463298824295a810f3ae217a
SafeShows <nils3nils@gmail.com>
2021-03-10 15:41:26 +0200

    ✨ add user to discord server

--- yourcontrols.xyz/app/Http/Controllers/AuthController.php
+++ yourcontrols.xyz/app/Http/Controllers/AuthController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
+use RestCord\DiscordClient;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login()
     {
-        return Socialite::driver('discord')->redirect();
+        return Socialite::driver('discord')
+        ->setScopes(['identify','email','guilds.join'])
+        ->redirect();
     }
 
     public function loginCallback()
@@ -19,17 +22,19 @@ class AuthController extends Controller
         $userData = Socialite::driver('discord')->user();
         $user = $this->getUser($userData);
         Auth::login($user);
+        // return Auth::user();
         return redirect()->route('member-area');
     }
 
     function getUser($userData)
     {
-        if ($user = User::find((int) $userData->id)) {
+        if ($user = User::where("discord_id", $userData->id)->first()) {
             $user = $this->updateUser($userData, $user);
             return $user;
         }else{
+            $discord = new DiscordClient(['token' => env('DISCORD_BOT_TOKEN')]);
             $user = new User();
-            $user->id = (int) $userData->id;
+            $user->discord_id = $userData->id;
             $user->avatar = $userData->avatar;
             $user->token = $userData->token;
             $user->refreshToken = $userData->refreshToken;
@@ -37,6 +42,11 @@ class AuthController extends Controller
             $user->email = $userData->email;
             $user->username = $userData->nickname;
             $user->save();
+            $discord->guild->addGuildMember([
+                "guild.id" => 764805300229636107,
+                "user.id" => (int) $userData->id,
+                "access_token" => $userData->token
+            ]);
             return $user;
         }
     }
@@ -49,6 +59,7 @@ class AuthController extends Controller
         $user->expiresIn = $userData->expiresIn;
         $user->email = $userData->email;
         $user->username = $userData->nickname;
+        $user->save();
         return $user;
     }
 
