162f332f106962b0b84769d10c17ffb339743ddc
SafeShows <nils3nils@gmail.com>
2021-04-19 07:12:14 +0300

    feat(status.yourcontrols.xyz):time period

--- status.yourcontrols.xyz/src/App.vue
+++ status.yourcontrols.xyz/src/App.vue
@@ -6,8 +6,19 @@
         Cloud Server Usage Chart
       </h2>
     </div>
+
+    <div class="selector">
+      Time period:
+      <select name="time_period" v-model="time_period" @change.stop="changePeriod">
+        <option value="24h">Last 24h</option>
+        <option value="week">Last week</option>
+        <option value="month">Last month</option>
+        <option value="all">All time</option>
+      </select>
+    </div>
+
     <div class="chart" v-if="!loading">
-      <apexchart type="area" height="500px" width="100%" :options="chartOptions" :series="seriesComputed"></apexchart>
+      <apexchart ref="chart" type="area" height="500px" width="100%" :options="chartOptions" :series="seriesComputed"></apexchart>
     </div>
     <div class="chart" v-else>
       Loading Chart...
@@ -21,6 +32,7 @@ import Vue from 'vue'
 export default Vue.extend({
   data: ():any => {
     return {
+      time_period: "24h",
       loading: true as boolean,
       series: [
         {
@@ -60,7 +72,7 @@ export default Vue.extend({
     }
   },
   created() {
-    axios.get("https://api.yourcontrols.xyz/stats").then(resoult=>{
+    axios.get("https://api.yourcontrols.xyz/stats/"+ this.time_period).then(resoult=>{
       resoult.data.forEach((element: { p2p: number; host: number; timestamp: string }) => {
         this.series[0].data.push(element.p2p)
         this.series[1].data.push(element.host)
@@ -77,6 +89,22 @@ export default Vue.extend({
       var sign = offset < 0? '+' : '-';
       offset = Math.abs(offset);
       return {h: z(offset/60 | 0), m: z(offset%60)};
+    },
+    changePeriod() {
+      this.loading = true
+      axios.get("https://api.yourcontrols.xyz/stats/" + this.time_period ).then(resoult=>{
+      this.series[0].data = []
+      this.series[1].data = []
+      this.chartOptions.xaxis.categories = []
+      resoult.data.forEach((element: { p2p: number; host: number; timestamp: string }) => {
+        this.series[0].data.push(element.p2p)
+        this.series[1].data.push(element.host)
+        let time = element.timestamp.split(" ", 2)
+        this.chartOptions.xaxis.categories.push(time[0]+"T"+time[1]+ "+0200")
+      })
+      this.loading = false
+      this.$refs.chart.refresh()
+    })
     }
   },
   computed: {
@@ -94,6 +122,7 @@ export default Vue.extend({
     height: 500px;
     margin: auto;
   }
+
   .img {
     width: 100%;
     margin: auto;
@@ -163,6 +192,11 @@ export default Vue.extend({
       width: 1200px;
       margin: auto;
       text-align: center;
+    }
+    .selector {
+      width: 1200px;
+      margin: auto;
+      text-align: left;
     }
   }
 }
