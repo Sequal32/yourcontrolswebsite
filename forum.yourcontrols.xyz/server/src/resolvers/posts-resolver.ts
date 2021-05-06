d8ff02b27c0976dae90c336540ba8a12b545d57f
SafeShows <nils3nils@gmail.com>
2021-05-06 15:42:46 +0300

    feat(forum)post resoulver

--- forum.yourcontrols.xyz/server/src/resolvers/posts-resolver.ts
+++ forum.yourcontrols.xyz/server/src/resolvers/posts-resolver.ts
@@ -1,16 +1,21 @@
 import { Repository } from 'typeorm';
 import { InjectRepository } from 'typeorm-typedi-extensions';
-import { Resolver, Query, Authorized } from 'type-graphql';
-import { Post } from '../entities/Post';
+import { Resolver, Query, Authorized, Arg } from 'type-graphql';
+import { Post } from '../entities';
 
 @Resolver(() => Post)
 export class PostResolver {
-  constructor(
-    @InjectRepository(Post) private readonly postRepository: Repository<Post>
-  ) {}
-  @Authorized()
-  @Query(() => [Post])
-  posts(): Promise<Post[]> {
-    return this.postRepository.find();
-  }
+    constructor(
+        @InjectRepository(Post) private readonly postRepository: Repository<Post>
+    ) {}
+    @Authorized()
+    @Query(() => [Post])
+    posts(): Promise<Post[]> {
+        return this.postRepository.find();
+    }
+    @Authorized()
+    @Query(() => Post)
+    post(@Arg("id") id: number): Promise<Post> {
+        return this.postRepository.findOne(id);
+    }
 }
